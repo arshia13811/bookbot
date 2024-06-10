21917bf2c1c74a4f953d087ca586aa8672c3504c
Arshia <arshiaswe@gmail.com>
2024-06-10 04:22:07 +0200

    main.py set up

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 def main():
-    path = "books/frankenstein.txt"
+    path = "/home/arshiadev/workspace/github.com/arshia13811/bookbot/books/frankenstein.txt"
     text = get_book_content(path)    
     num_words = words_counter(text)
     num_chars = char_counter(text)
-    print(f"{num_chars} chars found in the document")
+    x = char_counter(text)
+    for i in x:
+        print(i+"\n")
     
-
+    
+    
 def get_book_content(path):
     with open(path) as f:
         return f.read()
@@ -19,13 +22,18 @@ def words_counter(text):
 def char_counter(text):
     dic = {}
     words = text.lower()
-
     for i in words:
-        if i not in dic:
-            dic[i]=1
-        else:
-            dic[i]+=1
-        
+        if i.isalpha():
+            if i not in dic:
+                dic[i]=1
+            else:
+                dic[i]+=1
+                
+    sorted_items = sorted(dic.items(), key=lambda item: item[1], reverse=True)
+    lis = [f"the character {k} has {v} occurrences" for k, v in sorted_items]
     
+    return lis
+          
+
 if __name__ == "__main__":
     main()
